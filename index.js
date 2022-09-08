2052120f39699890f7d366e0f3ce6772af58a5ad
InTheRiches <72281809+InTheRiches@users.noreply.github.com>
2022-09-08 11:23:17 -0400

    added 24 hour time

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function createTimestamp(date) {
+function createTimestamp(date, hourTime) {
     const now = new Date();
     
     if (now.getTime() - date.getTime() <= 60000) {
@@ -12,14 +12,15 @@ function createTimestamp(date) {
     
     if (now.getTime() - date.getTime() <= 86400000 && now.getTime() - date.getTime() >= 0) {
         const hoursAgo = now.getHours() - date.getHours();
+        if(date.getHours() > 12 && !hourTime) hoursAgo -= 12;
         return hoursAgo === 1 ? hoursAgo + " hour ago." : "Today at " + date.getHours() + ":" + (date.getMinutes > 9 ? date.getMinutes() : "0" + date.getMinutes());
     }
     
     if (now.getDay() - date.getDay() === 1 && now.getMonth() === date.getMonth() && now.getYear() === date.getYear()) {
-        return "Yesterday at " + date.getHours() + ":" + (date.getMinutes > 9 ? date.getMinutes() : "0" + date.getMinutes());
+        return "Yesterday at " + (date.getHours() > 12 && !hourTime ? date.getHours()-12 : date.getHours()) + ":" + (date.getMinutes > 9 ? date.getMinutes() : "0" + date.getMinutes());
     }
     
     return date.getMonth() + "/" + date.getDay() + "/" + (1900 + date.getYear());
 }
 
-console.log(createTimestamp(new Date(2021, 8, 8, 10, 5, 0, 0)));
+console.log(createTimestamp(new Date(2021, 8, 8, 10, 5, 0, 0), true));
