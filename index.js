e424e41c803899fa131433e9993683326d6780f7
InTheRiches <72281809+InTheRiches@users.noreply.github.com>
2022-09-08 11:17:04 -0400

    Added a basic timestamp system

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
+function createTimestamp(date) {
+    const now = new Date();
+    
+    if (now.getTime() - date.getTime() <= 60000) {
+        return "Just now.";
+    }
 
+    if (now.getTime() - date.getTime() <= 3600000 && now.getTime() - date.getTime() >= 0) {
+        const minutesAgo = now.getMinutes() - date.getMinutes();
+        return minutesAgo === 1 ? minutesAgo + " minute ago." : minutesAgo + " minutes ago.";
+    }
+    
+    if (now.getTime() - date.getTime() <= 86400000 && now.getTime() - date.getTime() >= 0) {
+        const hoursAgo = now.getHours() - date.getHours();
+        return hoursAgo === 1 ? hoursAgo + " hour ago." : "Today at " + date.getHours() + ":" + (date.getMinutes > 9 ? date.getMinutes() : "0" + date.getMinutes());
+    }
+    
+    if (now.getDay() - date.getDay() === 1 && now.getMonth() === date.getMonth() && now.getYear() === date.getYear()) {
+        return "Yesterday at " + date.getHours() + ":" + (date.getMinutes > 9 ? date.getMinutes() : "0" + date.getMinutes());
+    }
+    
+    return date.getMonth() + "/" + date.getDay() + "/" + (1900 + date.getYear());
+}
+
+console.log(createTimestamp(new Date(2021, 8, 8, 10, 5, 0, 0)));
